a4adaec39206770543981cb544e9fbc7d8402127
Csurek <turekb@gmail.com>
2016-04-18 13:01:25 +0200

    ignore update

---
+++ AarhusWebDevelopers/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AarhusWebDevelopers")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AarhusWebDevelopers")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b2b297ab-2292-432f-b967-93e3c10b645d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ AarhusWebDevelopers/ViewModels/ContactForm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace AarhusWebDevelopers.ViewModels
+{
+    public class ContactForm
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress(ErrorMessage = "This is not a valid e-mail address")]
+        public string Email { get; set; }
+
+        [Required]
+        public string Subject { get; set; }
+
+        [Required]
+        public string Message { get; set; }
+    }
+}
